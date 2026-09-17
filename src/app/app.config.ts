797df392295018e55e30546ff0d21be64f43e88f
Manolo <manueldl12062003@gmail.com>
2026-09-17 14:18:02 +0200

    created api client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { API_BASE_URL } from '@shared/api/api-config';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: API_BASE_URL,
+      useValue: 'localhost:8080/api'
+    }
+  ],
 };
